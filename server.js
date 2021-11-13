31c25e4916f116025a3ecc2cd64d82f9ee63058a
clean_water <shmizuyuta213@icloud.com>
2021-11-14 01:37:36 +0900

    heroku エラー修正

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(cors());
 //   res.send(200);
 // });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+  res.sendFile(path.join(__dirname,'/client/build/index.html'));
 });
 
 
