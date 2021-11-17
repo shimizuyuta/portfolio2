0a1dfa3050100aa136b781c6d71fc58d1a634751
clean_water <shmizuyuta213@icloud.com>
2021-11-17 11:57:20 +0900

    言語・レベルフォーム

--- client/src/component/Select/LangStar.js
+++ client/src/component/Select/LangStar.js
@@ -38,71 +38,71 @@ const LangStart = () => {
 
   const classes = useStyles();
   return (
-  <Grid container direction="row"   justifyContent="center"  className={classes.skillsForm}>
+  <Grid container direction="row"   justifyContent="space-between"  className={classes.skillsForm}>
     <Grid item xs={5}  >
-  <FormControl variant="outlined" className={classes.langForm}>
-    <Select
-      native
-      value={state.langStart}
-      onChange={handleChange}
-      inputProps={{
-        name: 'langStart',
-        id: 'outlined-langStart-native-simple',
-      }}
-    >
-      <option aria-label="None" value="" />
-      <option value={1}>ActionScript</option>
-		  <option value={2}>AIR</option>
-		  <option value={3}>Androidアプリ</option>
-		  <option value={4}>ASP.net</option>
-		  <option value={5}>C#</option>
-		  <option value={6}>COBOL</option>
-		  <option value={7}>CSS</option>
-		  <option value={8}>C／C++</option>
-		  <option value={9}>Delphi</option>
-		  <option value={10}>Facebookアプリ</option>
-		  <option value={11}>Fortran</option>
-		  <option value={12}>Go</option>
-		  <option value={13}>HTML</option>
-		  <option value={14}>iPhoneアプリ</option>
-		  <option value={15}>Java</option>
-		  <option value={16}>JavaScript</option>
-		  <option value={17}>MATLAB</option>
-		  <option value={18}>MySQL</option>
-		  <option value={19}>Objective-C</option>
-		  <option value={20}>Open CV</option>
-		  <option value={21}>OpenGL</option>
-		  <option value={22}>Oracle</option>
-		  <option value={23}>Pascal</option>
-		  <option value={24}>Perl</option>
-		  <option value={25}>PHP</option>
-		  <option value={26}>Postgres</option>
-		  <option value={27}>Python</option>
-		  <option value={28}>Ruby</option>
-		  <option value={29}>R言語</option>
-		  <option value={30}>Scala</option>
-		  <option value={31}>SQL</option>
-		  <option value={32}>Swift</option>
-		  <option value={33}>Unity</option>
-		  <option value={34}>VB</option>
-		  <option value={35}>VB .net</option>
-		  <option value={36}>VBA</option>
-		  <option value={37}>VC</option>
-    </Select>
-  </FormControl>
-  </Grid>
-  <Grid item xs={5} >
-  <Box component="fieldset" mb={3} borderColor="transparent">
-    <Rating
-      name="simple-controlled"
-      value={value}
-      onChange={(event, newValue) => {
-        console.log(newValue,'value start')
-        setValue(newValue);
-      }}
-    />
-  </Box>
-  </Grid>
+      <FormControl variant="outlined" className={classes.langForm}>
+        <Select
+          native
+          value={state.langStart}
+          onChange={handleChange}
+          inputProps={{
+            name: 'langStart',
+            id: 'outlined-langStart-native-simple',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={1}>ActionScript</option>
+	    	  <option value={2}>AIR</option>
+	    	  <option value={3}>Androidアプリ</option>
+	    	  <option value={4}>ASP.net</option>
+	    	  <option value={5}>C#</option>
+	    	  <option value={6}>COBOL</option>
+	    	  <option value={7}>CSS</option>
+	    	  <option value={8}>C／C++</option>
+	    	  <option value={9}>Delphi</option>
+	    	  <option value={10}>Facebookアプリ</option>
+	    	  <option value={11}>Fortran</option>
+	    	  <option value={12}>Go</option>
+	    	  <option value={13}>HTML</option>
+	    	  <option value={14}>iPhoneアプリ</option>
+	    	  <option value={15}>Java</option>
+	    	  <option value={16}>JavaScript</option>
+	    	  <option value={17}>MATLAB</option>
+	    	  <option value={18}>MySQL</option>
+	    	  <option value={19}>Objective-C</option>
+	    	  <option value={20}>Open CV</option>
+	    	  <option value={21}>OpenGL</option>
+	    	  <option value={22}>Oracle</option>
+	    	  <option value={23}>Pascal</option>
+	    	  <option value={24}>Perl</option>
+	    	  <option value={25}>PHP</option>
+	    	  <option value={26}>Postgres</option>
+	    	  <option value={27}>Python</option>
+	    	  <option value={28}>Ruby</option>
+	    	  <option value={29}>R言語</option>
+	    	  <option value={30}>Scala</option>
+	    	  <option value={31}>SQL</option>
+	    	  <option value={32}>Swift</option>
+	    	  <option value={33}>Unity</option>
+	    	  <option value={34}>VB</option>
+	    	  <option value={35}>VB .net</option>
+	    	  <option value={36}>VBA</option>
+	    	  <option value={37}>VC</option>
+        </Select>
+      </FormControl>
+    </Grid>
+    <Grid item xs={5} >
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            console.log(newValue,'value start')
+            setValue(newValue);
+          }}
+        />
+      </Box>
+    </Grid>
   </Grid>
   )
 }
