843422be7160e16b05d53f0f7f4b13f89f379670
clean_water <shmizuyuta213@icloud.com>
2021-11-22 02:19:08 +0900

    nicknameからusernameに変更

--- client/src/component/Select/UserName.js
+++ client/src/component/Select/UserName.js
@@ -2,24 +2,17 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles((theme) => ({
-}));
-
-export default function NickName() {
-  const classes = useStyles();
 
 
-  const [name, setName] = React.useState('');
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
+export default function username(props) {
 
   return (
     <form noValidate autoComplete="off">
       <TextField 
       id="outlined-basic" 
-      value={name}
-      onChange={handleChange}
+      name="username"
+      value={props.username}
+      onChange={props.handleChange}
       variant="outlined"
       fullWidth
     />
