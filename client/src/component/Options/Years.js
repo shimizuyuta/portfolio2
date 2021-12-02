84be5c130970a348dd7047032c67b9aa8a61155a
clean_water <shmizuyuta213@icloud.com>
2021-12-02 21:07:17 +0900

    学部・言語・地域・学年オブジェクト作成

---
+++ client/src/component/Options/Years.js
@@ -0,0 +1,10 @@
+const years = [
+  {name:'指定なし',value:0},
+  {name:'高校生',value:1},
+  {name:'専門・大学１年生',value:2},
+  {name:'専門・大学２年生',value:3},
+  {name:'専門・大学３年生',value:4},
+  {name:'専門・大学４年生',value:5},
+]
+
+export default years
